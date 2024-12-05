0693b6fd71cfc6009bae771c58a866c25b7c83c8
rynong99 <ryon5157@colorado.edu>
2024-12-05 00:22:49 -0700

    Map Elements changes
    Changed food spawn locations and speed.

--- map_elements/SpawnFood.gd
+++ map_elements/SpawnFood.gd
@@ -1,16 +1,16 @@
 extends Node3D
 
 @onready var food = [preload("res://map_elements/sphere_food.tscn"), preload("res://map_elements/cube_food.tscn"), preload("res://map_elements/capsule_food.tscn")]
-@export var spawn_rate := 10.0
+@export var spawn_rate := 0.1
 var time_since_spawn := 0.0
-
+var prev_point = Vector3.ZERO
 var spawn_points = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for child in get_children():
 		spawn_points.append(child.global_position)
-	
+		print(child)
 	spawn_food()
 	pass
 
@@ -23,6 +23,12 @@ func _physics_process(delta):
 
 func spawn_food():
 	var spawn_point = spawn_points[randi() % spawn_points.size()]
-	var drop = food[randi() % food.size()].instantiate()
-	drop.global_position = spawn_point
-	add_child(drop)
+	print(spawn_point)
+	print(prev_point)
+	if spawn_point != prev_point:
+		var drop = food[randi() % food.size()].instantiate()
+		drop.global_position = spawn_point
+		add_child(drop)
+		prev_point = spawn_point
+	else:
+		pass
--- map_elements/food.gd
+++ map_elements/food.gd
@@ -1,8 +1,9 @@
 extends RigidBody3D
 
-var lifespan := 0
+var lifespan := 0.0
 
-const MAX_LIFE = 10.0
+const MAX_LIFE = 20.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,9 +12,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	lifespan += delta
+	if (lifespan <= 1.5):
+		var tween = create_tween()
+		self.freeze = true
+		tween.tween_property($CollisionShape3D, "scale", Vector3.ONE, 1.0)
+		await tween.finished
+		self.freeze = false
 	if (lifespan > MAX_LIFE):
 		var tween = create_tween()
-		tween.tween_property($Capsule/CollisionShape3D, "scale", Vector3.ZERO, 0.5).set_trans(Tween.TRANS_ELASTIC)
+		tween.tween_property($CollisionShape3D, "scale", Vector3.ZERO, 1.0)
 		await tween.finished
 		queue_free()
 	pass
